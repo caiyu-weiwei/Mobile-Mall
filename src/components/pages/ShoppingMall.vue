87fc557c6b3b605b68a12bc47c900d997201f9a4
caiweiwei <caiweiwei@yazuo.com>
2018-04-28 15:05:53 +0800

    banner、lazyload

--- src/components/pages/ShoppingMall.vue
+++ src/components/pages/ShoppingMall.vue
@@ -1,16 +1,25 @@
 <template>
-  <div class="search-bar">
-    <van-row>
-      <van-col span="3">
-        <img :src="location" class="location-icon">
-      </van-col>
-      <van-col span="16">
-        <input type="text" class="search-input">
-      </van-col>
-      <van-col span="5">
-        <van-button size="mini">查询</van-button>
-      </van-col>
-    </van-row>
+  <div>
+    <div class="search-bar">
+      <van-row>
+        <van-col span="3">
+          <img :src="location" class="location-icon">
+        </van-col>
+        <van-col span="16">
+          <input type="text" class="search-input">
+        </van-col>
+        <van-col span="5">
+          <van-button size="mini">查询</van-button>
+        </van-col>
+      </van-row>
+    </div>
+    <div class="banner-area">
+      <van-swipe :autoplay = "3000">
+        <van-swipe-item v-for="(banner, index) in bannerArray" :key="index" v-lazy="banner">
+          <img :src="banner.imageUrl" alt="banner">
+        </van-swipe-item>
+      </van-swipe>
+    </div>
   </div>
 </template>
 
@@ -18,7 +27,12 @@
 export default {
   data () {
     return {
-      location: require('../../assets/images/location.png')
+      location: require('../../assets/images/location.png'),
+      bannerArray: [
+        {imageUrl: 'http://7xjyw1.com1.z0.glb.clouddn.com/simleVueDemoPic001.jpg'},
+        {imageUrl: 'http://7xjyw1.com1.z0.glb.clouddn.com/simleVueDemoPic002.jpg'},
+        {imageUrl: 'http://7xjyw1.com1.z0.glb.clouddn.com/simleVueDemoPic003.jpg'}
+      ]
     }
   }
 }
@@ -29,6 +43,7 @@ export default {
   height: 2.2rem;
   background-color: #e5017d;
   line-height: 2.2rem;
+  overflow: hidden;
 }
 .location-icon{
   width: 80%;
@@ -41,5 +56,12 @@ export default {
   border-bottom: 1px solid #fff;
   background-color: #e5017d;
   color: #fff;
+}
+.banner-area{
+  clear: both;
+  max-height: 10rem;
+}
+.banner-area img{
+  width: 100%;
 }
 </style>
